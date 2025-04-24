907d7ea16a612ecfff5921a96d4a487d0e2b9bf7
araamat <raamatandrus@gmail.com>
2025-04-24 16:26:32 +0300

    search by line korrastamine

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from zoneinfo import ZoneInfo
 import requests
 
 # ✅ GTFS allalaadimise seadistus
-GTFS_ZIP = "26022025.zip"
+GTFS_ZIP = "gtfs.zip"
 GTFS_URL = "https://peatus.ee/gtfs/gtfs.zip"
 
 def needs_update(filepath, hours=24):
@@ -42,24 +42,19 @@ def local_css(file_name):
 
 local_css("style.css")
 
-# Menüünavigatsioon
-st.sidebar.title("Menüü")
-page = st.sidebar.selectbox("Vali leht", [
-    "Avaleht", 
-    "Route ID", 
-    "Trip ID", 
-    "Liini number", 
-    "Authority"
-])
-
-# Vaate valik
-if page == "Avaleht":
-    st.title("Tere tulemast GTFS andmete vaaturisse! 👋")
-
+# Sidebar
+with st.sidebar:
+    st.title("GTFS Vaatur")
     if os.path.exists(GTFS_ZIP):
         est_time = datetime.fromtimestamp(os.path.getmtime(GTFS_ZIP), ZoneInfo("Europe/Tallinn"))
-        st.info(f"📅 Andmestik uuendati: `{est_time.strftime('%Y-%m-%d %H:%M:%S')}` (Eesti aeg)")
+        st.caption(f"📅 Viimati uuendatud: {est_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
+    go_home = st.button("🏠 Avaleht")
+    page = st.selectbox("📂 Vali otsingu alus", ["", "Liini number", "Peatused",  "Route ID", "Trip ID"])
+
+# Avaleht kuvatakse nupu või tühja valiku puhul
+if go_home or not page:
+    st.title("Tere tulemast Eesti ühistranspordi andmestiku (GTFS) analüüsi ja valideerimise tööriista ! 👋")
     st.markdown("""
         🚍 See rakendus võimaldab sul:
         - Otsida transpordiliine `route_id`, `trip_id` või liini numbri järgi  
@@ -67,7 +62,7 @@ if page == "Avaleht":
         - Näha visuaalselt kaardil marsruuti  
         - Kontrollida liinide teeninduspäevi ja erandeid  
 
-        👉 Kasutamiseks vali vasakult lehelt sobiv valik.
+        👉 Kasutamiseks vali vasakult lehelt sobiv andmetüüp.
     """)
 elif page == "Route ID":
     route_view()
@@ -75,5 +70,5 @@ elif page == "Trip ID":
     trip_view()
 elif page == "Liini number":
     line_view()
-elif page == "Authority":
+elif page == "Peatused":
     authority_view()
--- search_by_line.py
+++ search_by_line.py
@@ -59,9 +59,9 @@ def custom_sort(valikud, otsi):
 def gtfs_view():
     routes_df, trips_df, stop_times_df, stops_df, calendar_df, calendar_dates_df = load_data()
 
-    st.title("🚍 Otsi liini numbri järgi")
+    st.title("🚍 Liininumbri järgi otsing")
 
-    otsi = st.text_input("Sisesta liini number")
+    otsi = st.text_input("Sisesta liininumber, kui soovid, et järgmises lahtris sorteeritakse tulemuseid")
 
     # Sort loogika
     otsi_lower = otsi.lower()
@@ -77,7 +77,7 @@ def gtfs_view():
             others.append(v)
     sorted_routes = custom_sort(routes_df['valik'], otsi)
 
-    valik = st.selectbox("Vali liin", ["— Vali liin —"] + list(sorted_routes if otsi else routes_df['valik']))
+    valik = st.selectbox("**Vali liin**", ["— Vali liin —"] + list(sorted_routes if otsi else routes_df['valik']))
 
 
     if valik and valik != "— Vali liin —":
@@ -86,23 +86,26 @@ def gtfs_view():
             st.warning("Valitud liini ei leitud.")
             return
         route_id = valik_df['route_id'].iloc[0]
-        st.markdown(f"### Valitud liin: **{valik}**  \nRoute ID: `{route_id}`")
+        st.markdown(f"### Valitud liin: **{valik}**  \n**Liini Route ID väärtus:** {route_id}")
 
         relevant_trips = trips_df[trips_df['route_id'] == route_id]
         if not relevant_trips.empty:
             relevant_trips['trip_label'] = relevant_trips['trip_id'].astype(str) + " (" + relevant_trips['trip_long_name'] + ")"
-            trip_valik = st.selectbox("Trip_id valik", relevant_trips['trip_label'])
+            trip_valik = st.selectbox("**Liini Trip ID valik:**", relevant_trips['trip_label'], key="trip_select",  placeholder="Vali trip",  label_visibility="visible")
             selected_trip = relevant_trips[relevant_trips['trip_label'] == trip_valik].iloc[0]
             trip_id = selected_trip['trip_id']
             service_id = selected_trip['service_id']
 
-            st.markdown(f"**Trip ID:** `{trip_id}`  **Trip long name:** {selected_trip['trip_long_name']}")
+            st.markdown(f"**Valitud Trip ID:** {trip_id} **Trip long name:** {selected_trip['trip_long_name']}")
 
             stop_seq = stop_times_df[stop_times_df['trip_id'] == trip_id].merge(stops_df, on="stop_id").sort_values("stop_sequence")
 
             if not stop_seq.empty:
-                st.write("### Peatused ja ajad")
-                st.dataframe(stop_seq[['stop_sequence', 'stop_name', 'arrival_time', 'departure_time']])
+                st.write("### Valitud liini Trip ID-ga seotud peatused ja nende andmestik")
+                cols = ['stop_sequence', 'stop_id', 'stop_name', 'stop_code', 'arrival_time', 'departure_time']
+                cols = [col for col in cols if col in stop_seq.columns]
+                stop_seq[cols] = stop_seq[cols].astype(str)
+                st.dataframe(stop_seq[cols], use_container_width=True, hide_index=True)
 
                 st.write("### Peatused kaardil")
                 m = folium.Map(location=[stop_seq.iloc[0]['stop_lat'], stop_seq.iloc[0]['stop_lon']], zoom_start=13)
@@ -112,29 +115,42 @@ def gtfs_view():
                         popup=row['stop_name'],
                         icon=folium.Icon(color='blue', icon='info-sign')
                     ).add_to(m)
-                st_folium(m, height=400)
+                with st.container():
+                 st_folium(m, height=400, width="100%")
 
-            st.write("### Teeninduspäevad")
+            st.write("### Teenindusperiood")
             cal = calendar_df[calendar_df['service_id'] == service_id]
             if not cal.empty:
                 cal_row = cal.iloc[0]
-                days = ", ".join([day.capitalize() for day in ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'] if cal_row[day] == 1])
+                day_map = {
+                    'monday': 'Esmaspäev',
+                    'tuesday': 'Teisipäev',
+                    'wednesday': 'Kolmapäev',
+                    'thursday': 'Neljapäev',
+                    'friday': 'Reede',
+                    'saturday': 'Laupäev',
+                    'sunday': 'Pühapäev'
+                }
+                days = ", ".join([day_map[day] for day in day_map if day in cal_row and cal_row[day] == 1])
+
+                
                 st.markdown(
-    f"- Algus: `{cal_row['start_date'].date()}`  \n"
-    f"- Lõpp: `{cal_row['end_date'].date()}`  \n"
-    f"- Päevad: {days}"
+    f"- **Algus**: {cal_row['start_date'].date()}  \n"
+    f"- **Lõpp**: {cal_row['end_date'].date()}  \n"
+    f"- **Liin on käigus**: {days}"
 )
 
-            else:
-                st.info("Teeninduspäevad puuduvad.")
 
-            st.write("### Erandid")
+            else:
+                st.info("**Teeninduspäevad puuduvad.**")
+
+            st.write("### Teenindusperioodi erandid")
             exceptions = calendar_dates_df[calendar_dates_df['service_id'] == service_id]
             if exceptions.empty:
-                st.info("Erandid puuduvad.")
+                st.info("**Erandid puuduvad.**")
             else:
                 for _, row in exceptions.iterrows():
-                    muutus = "Lisatud" if row['exception_type'] == 1 else "Tühistatud"
+                    muutus = "Lisatud" if row['exception_type'] == 1 else "Liin ei ole käigus."
                     st.markdown(f"- {row['date'].date()} — **{muutus}**")
         else:
             st.warning("Selle liiniga ei leitud sõite.")
